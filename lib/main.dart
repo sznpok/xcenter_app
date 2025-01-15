09f111c70bea0e31cc4cf6d3da10fe8838b776b8
sznpok <sijanpok55@gmail.com>
2025-01-15 18:13:26 +0545

    firebase configuration completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiktok_video/routers/routers.dart';
 
+import 'firebase_options.dart';
 import 'utils/navigator_key.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
